66cbc6dd13570af6e1615fd6c3f9ff4e61e98f4b
Thomas Schmidt <tnschmidt18@gmail.com>
2026-04-22 15:51:13 -0400

    (views) add home and crud views for tracks

--- Full-Stack_Data_Web_Application_with_Django/spotifyapp/views.py
+++ Full-Stack_Data_Web_Application_with_Django/spotifyapp/views.py
@@ -1,11 +1,14 @@
 import json
 
 import pandas as pd
+from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.core.management import call_command
+from django.urls import reverse
 from django.views.decorators.http import require_POST
+from .forms import TrackForm
 from .models import Track
 
 
@@ -133,3 +136,47 @@ def analytics_view(request):
 def fetch_data_view(request):
     call_command('fetch_data', limit=25)
     return redirect('/')
+
+
+def home(request):
+    stats = {
+        'total_tracks': Track.objects.count(),
+    }
+    return render(request, 'spotifyapp/home.html', {'stats': stats})
+
+
+def track_list(request):
+    tracks = Track.objects.select_related('genre').all()
+    paginator = Paginator(tracks, 20)
+    page_obj = paginator.get_page(request.GET.get('page'))
+    return render(request, 'spotifyapp/records_list.html', {'page_obj': page_obj})
+
+
+def track_detail(request, pk):
+    track = get_object_or_404(Track.objects.select_related('genre'), pk=pk)
+    return render(request, 'spotifyapp/detail.html', {'track': track})
+
+
+def track_create(request):
+    form = TrackForm(request.POST or None)
+    if form.is_valid():
+        track = form.save()
+        return redirect(reverse('track_detail', args=[track.pk]))
+    return render(request, 'spotifyapp/form.html', {'form': form, 'action': 'Add Track'})
+
+
+def track_update(request, pk):
+    track = get_object_or_404(Track, pk=pk)
+    form = TrackForm(request.POST or None, instance=track)
+    if form.is_valid():
+        form.save()
+        return redirect(reverse('track_detail', args=[track.pk]))
+    return render(request, 'spotifyapp/form.html', {'form': form, 'action': 'Edit Track', 'track': track})
+
+
+def track_delete(request, pk):
+    track = get_object_or_404(Track, pk=pk)
+    if request.method == 'POST':
+        track.delete()
+        return redirect('track_list')
+    return render(request, 'spotifyapp/confirm_delete.html', {'track': track})
